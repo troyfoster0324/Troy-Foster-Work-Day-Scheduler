f7138083d2e71d5ce9ef5775f432189b4c0cef3e
Troy <colorado_tf@yahoo.com>
2023-08-03 18:20:12 -0600

    Html config completed

--- docs/script.js
+++ docs/script.js
@@ -1,36 +1,165 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+var currentTimeDisplay = $("#currentDay");
+currentTimeDisplay.text(moment().format('llll'));
 
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+//Create variables for textarea to be used to get value
+var hourNineText = $("#hour-9");
+var hourTenText = $("#hour-10");
+var hourElevenText = $("#hour-11");
+var hourTwelveText = $("#hour-12");
+var hourThirteenText = $("#hour-13");
+var hourFourteenText = $("#hour-14");
+var hourFifteenText = $("#hour-15");
+var hourSixteenText = $("#hour-16");
+var hourSeventeenText = $("#hour-17");
+var confirmationMsg = $("#confirmation-msg");
+var timeBlock = $(".time-block")
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    }) 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//Create variables for save button to initialize a click event to store textarea value to local storage
+var saveBtn = $(".saveBtn");
+
+//Onclick, save the value of all items to local storage
+saveBtn.on("click", function(e) {
+    e.preventDefault();
+    confirmationMsg.html("Appointment Added with <code>localStorage</code> ✅");
+    confirmationMsg.attr("style", "text-align: center; font-size: 12px");
+    hourNineVal = hourNineText.val();
+    hourTenVal = hourTenText.val();
+    hourElevenVal = hourElevenText.val();
+    hourTwelveVal =  hourTwelveText.val();
+    hourThirteenVal = hourThirteenText.val(); 
+    hourFourteenVal = hourFourteenText.val(); 
+    hourFifteenVal = hourFifteenText.val();
+    hourSixteenVal = hourSixteenText.val();
+    hourSeventeenVal = hourSeventeenText.val();
+    localStorage.setItem("hour09", hourNineVal);
+    localStorage.setItem("hour10", hourTenVal);
+    localStorage.setItem("hour11", hourElevenVal);
+    localStorage.setItem("hour12", hourTwelveVal);
+    localStorage.setItem("hour13", hourThirteenVal);
+    localStorage.setItem("hour14", hourFourteenVal);
+    localStorage.setItem("hour15", hourFifteenVal);
+    localStorage.setItem("hour16", hourSixteenVal);
+    localStorage.setItem("hour17", hourSeventeenVal);
 });
+
+//On page load, place all items from local storage to pageset
+function init() {
+    hourNineText.text(localStorage.getItem("hour09"));
+    hourTenText.text(localStorage.getItem("hour10"));
+    hourElevenText.text(localStorage.getItem("hour11"));
+    hourTwelveText.text(localStorage.getItem("hour12"));
+    hourThirteenText.text(localStorage.getItem("hour13"));
+    hourFourteenText.text(localStorage.getItem("hour14"));
+    hourFifteenText.text(localStorage.getItem("hour15"));
+    hourSixteenText.text(localStorage.getItem("hour16"));
+    hourSeventeenText.text(localStorage.getItem("hour17"));
+};
+
+init()
+
+
+//Find value for current hour
+var currentHour = moment().hours();
+// var currentHour = 12
+// console.log(currentHour);
+
+for (var i = 0; i < timeBlock.length; i++) {
+    if (timeBlock[i].dataset.time == currentHour) {
+        timeBlock[i].classList.remove("past");
+        timeBlock[i].classList.remove("future");
+        timeBlock[i].classList.add("present");
+    };
+    if (timeBlock[i].dataset.time > currentHour) {
+        timeBlock[i].classList.remove("past");
+        timeBlock[i].classList.remove("present");
+        timeBlock[i].classList.add("future");
+    };
+    if (timeBlock[i].dataset.time < currentHour) {
+        timeBlock[i].classList.remove("present");
+        timeBlock[i].classList.remove("future");
+        timeBlock[i].classList.add("past");
+    }
+};var currentTimeDisplay = $("#currentDay");
+currentTimeDisplay.text(moment().format('llll'));
+
+//Create variables for textarea to be used to get value
+var hourNineText = $("#hour-9");
+var hourTenText = $("#hour-10");
+var hourElevenText = $("#hour-11");
+var hourTwelveText = $("#hour-12");
+var hourThirteenText = $("#hour-13");
+var hourFourteenText = $("#hour-14");
+var hourFifteenText = $("#hour-15");
+var hourSixteenText = $("#hour-16");
+var hourSeventeenText = $("#hour-17");
+var confirmationMsg = $("#confirmation-msg");
+var timeBlock = $(".time-block")
+
+
+//Create variables for save button to initialize a click event to store textarea value to local storage
+var saveBtn = $(".saveBtn");
+
+//Onclick, save the value of all items to local storage
+saveBtn.on("click", function(e) {
+    e.preventDefault();
+    confirmationMsg.html("Appointment Added with <code>localStorage</code> ✅");
+    confirmationMsg.attr("style", "text-align: center; font-size: 12px");
+    hourNineVal = hourNineText.val();
+    hourTenVal = hourTenText.val();
+    hourElevenVal = hourElevenText.val();
+    hourTwelveVal =  hourTwelveText.val();
+    hourThirteenVal = hourThirteenText.val(); 
+    hourFourteenVal = hourFourteenText.val(); 
+    hourFifteenVal = hourFifteenText.val();
+    hourSixteenVal = hourSixteenText.val();
+    hourSeventeenVal = hourSeventeenText.val();
+    localStorage.setItem("hour09", hourNineVal);
+    localStorage.setItem("hour10", hourTenVal);
+    localStorage.setItem("hour11", hourElevenVal);
+    localStorage.setItem("hour12", hourTwelveVal);
+    localStorage.setItem("hour13", hourThirteenVal);
+    localStorage.setItem("hour14", hourFourteenVal);
+    localStorage.setItem("hour15", hourFifteenVal);
+    localStorage.setItem("hour16", hourSixteenVal);
+    localStorage.setItem("hour17", hourSeventeenVal);
+});
+
+//On page load, place all items from local storage to pageset
+function init() {
+    hourNineText.text(localStorage.getItem("hour09"));
+    hourTenText.text(localStorage.getItem("hour10"));
+    hourElevenText.text(localStorage.getItem("hour11"));
+    hourTwelveText.text(localStorage.getItem("hour12"));
+    hourThirteenText.text(localStorage.getItem("hour13"));
+    hourFourteenText.text(localStorage.getItem("hour14"));
+    hourFifteenText.text(localStorage.getItem("hour15"));
+    hourSixteenText.text(localStorage.getItem("hour16"));
+    hourSeventeenText.text(localStorage.getItem("hour17"));
+};
+
+init()
+
+
+//Find value for current hour
+var currentHour = moment().hours();
+// var currentHour = 12
+// console.log(currentHour);
+
+for (var i = 0; i < timeBlock.length; i++) {
+    if (timeBlock[i].dataset.time == currentHour) {
+        timeBlock[i].classList.remove("past");
+        timeBlock[i].classList.remove("future");
+        timeBlock[i].classList.add("present");
+    };
+    if (timeBlock[i].dataset.time > currentHour) {
+        timeBlock[i].classList.remove("past");
+        timeBlock[i].classList.remove("present");
+        timeBlock[i].classList.add("future");
+    };
+    if (timeBlock[i].dataset.time < currentHour) {
+        timeBlock[i].classList.remove("present");
+        timeBlock[i].classList.remove("future");
+        timeBlock[i].classList.add("past");
+    }
+};
